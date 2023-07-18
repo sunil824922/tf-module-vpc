f0089e31ef6172dc24cac2c9105e1a940027e1ff
sunil <sunilkumar.ipm@gmail.com>
2023-07-18 17:52:52 +0530

    vpc

--- main.tf
+++ main.tf
@@ -26,20 +26,16 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_eip" "ngw" {
    count = length(lookup(lookup(var.subnets, "public", null), "cidr_block", 0 ))
 #  count = length(var.subnets["public"].cidr_block)
-  vpc      = true
-  tags = merge(var.tags, {Name = "${var.env}-ngw" })
+   vpc      = true
+   tags = merge(var.tags, {Name = "${var.env}-ngw" })
 }
 
-#resource "aws_nat_gateway" "ngw" {
-#  count = length(var.subnets["public"].cidr_block)
-#  allocation_id =  aws_eip.ngw[count.index].id
-#
-#
-#subnet_id     =  module.subnets["public"].subnet_ids[count.index]
-#
-#tags = merge(var.tags, {Name = "${var.env}-ngw" })
-#}
+resource "aws_nat_gateway" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id =  aws_eip.ngw[count.index].id
 
-output "subnet_ids" {
-  value = "module.subnets"
+
+subnet_id     =  module.subnets["public"].subnet_ids[count.index]
+
+tags = merge(var.tags, {Name = "${var.env}-ngw" })
 }
