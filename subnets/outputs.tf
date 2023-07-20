04137f871c0deda9c5180ac2898faec4f1a8a261
sunil <sunilkumar.ipm@gmail.com>
2023-07-20 12:40:46 +0530

    aws_route to public

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -2,8 +2,15 @@ output "subnet_ids" {
   value = aws_subnet.main.*.id
 }
 
+output "subnet_cidr" {
+  value = aws_subnet.main.*.cidr_block
+}
+
+
 output "route_table_ids" {
   value = aws_route_table.main.*.id
 }
+
+
 
 
